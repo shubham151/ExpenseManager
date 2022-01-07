cf1a804ca9d96c66c333628aa4bceddfc1153516
Shubham Mishra <shubham.cmishra@gmail.com>
2022-01-08 01:24:03 +0530

    return changes

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,21 +1,19 @@
 import ExpenceItem from "./ExpenceItem";
-import '../css/Expense.css'
+import "../css/Expense.css";
 
 function Expense(props) {
   let data = [];
   const expense = props.expense;
-  
   for (let itr in expense) {
     data.push(
       <ExpenceItem
         title={expense[itr].title}
         amount={expense[itr].amount}
         date={expense[itr].date}
-      >
-      </ExpenceItem> 
+      ></ExpenceItem>
     );
   }
-  return data;
+  return <div className="expenses">{data}</div>;
 }
 
 export default Expense;
